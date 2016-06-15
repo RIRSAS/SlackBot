61542a1074bf1d94333e709592eb426f9155461a
RIRSAS <soratosyo9@gmail.com>
2016-06-15 23:28:49 +0900

    First comit.

---
+++ slackbot.js
@@ -0,0 +1,27 @@
+/*
+For Bot Framework with Slack
+*/
+
+//var Botkit = require('botkit');
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var port = process.env.PORT || 8080;
+
+// Create bot
+var bot = new builder.BotConnectorBot({
+	appId: '',
+	appSecret: ''
+});
+
+// Talk Method
+bot.add('/', function (session) {
+   session.send('Hello World'); 
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(port, function() {
+	console.log('%s listening to %s', server.name, server.url);
+});
