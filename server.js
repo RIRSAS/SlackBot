d7d025ca2427abb7e1deb451fff8f70295007a76
RIRSAS <soratosyo9@gmail.com>
2016-06-16 22:12:55 +0900

    bot update

---
+++ server.js
@@ -0,0 +1,24 @@
+/*
+For Bot Framework with Slack
+*/
+
+//var Botkit = require('botkit');
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var port = process.env.PORT || 8080;
+
+// Create bot
+var bot = new builder.BotConnectorBot({appId:'slack-bot-rirsas', appSecret:'slack-bot-rirsas'});
+
+// Talk Method
+bot.add('/', function (session) {
+   session.send('Hello World'); 
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(port, function() {
+	console.log('%s listening to %s', server.name, server.url);
+});
