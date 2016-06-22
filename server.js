d8eaf9b4f004d1860cbae109451229e08ee8dfd1
RIRSAS <soratosyo9@gmail.com>
2016-06-22 22:03:11 +0900

    return receive message

--- server.js
+++ server.js
@@ -11,10 +11,26 @@ var port = process.env.PORT || 8080;
 // Create bot
 var bot = new builder.BotConnectorBot({appId:'slack-bot-rirsas', appSecret:'slack-bot-rirsas'});
 
+var dialog = new builder.CommandDialog();
+
+var greetingAry = ['よう','やあ','やぁ','こんにちわ','ハロー','おっす','こんばんわ','おはよう','ヘイ','hi','ヘロー','hello'];
+
+// オウム返しテスト
+dialog.matches(greetingAry,	function(session){
+// dialog.matches(['よう','やあ','やぁ','こんにちわ','ハロー','おっす','こんばんわ','おはよう','ヘイ','hi','ヘロー','hello'],	 function(session){
+			session.send(session.message.text);
+			// session.send("hi! I'm Bot");
+	}
+);
+
 // Talk Method
-bot.add('/', function (session) {
-   session.send('Hello World'); 
-});
+
+// bot.add('/', function (session) {
+//    session.send('Hello World'); 
+// });
+
+
+bot.add('/', dialog);
 
 // Setup Restify Server
 var server = restify.createServer();
