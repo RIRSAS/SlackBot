84fab2ebe0f536230d529242d87e42eeecf41f00
RIRSAS <soratosyo9@gmail.com>
2016-06-26 21:05:30 +0900

    Refactaring 201606262105

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ var client = require('cheerio-httpcli');
 // 日付取得用ライブラリ
 require('date-utils');
 
+// var importTest = require('./croring.js');
+// var testModule = importTest.test();
+// var yit = importTest.yahooIT();
+
 var port = process.env.PORT || 8080;
 
 // Create bot
@@ -29,23 +33,24 @@ var greetingAry = ['よう','やあ','やぁ','こんにちわ','ハロー','お
 // 	}
 // );
 
+// // 呼び出しテスト
+// dialog.matches('test',	function(session){
 
+// 		session.send(yit);
+// 		// session.send(session.message.text);
+// 	}
+// );
 
 // yahooニュースITカテゴリの先頭ページのテキストとリンクを表示
 dialog.matches('ニュース',
 	function(session){
-		// yahooニュースのITカテゴリへアクセス
-		// client.fetch('http://news.yahoo.co.jp/list/?c=computer',  function (err, $, res) {
 		// yahooのIT・科学ニュースの1ページ目へアクセス
 		client.fetch('http://news.yahoo.co.jp/hl?c=c_sci&p=1',  function (err, $, res) {
 
 		 var resultText = "Yahoo Japan IT・科学の新着ニュースは...\n\r";
 		 resultText += "---\n\r";
 		 
-		 // listクラスを持つul要素 の li要素 の a要素を対象としてeachを実行
-		 // $('ul.list > li > a').each(function(){
-		 // $('ul.listBd > li > p.ttl > a').each(function(){
-		// listクラスを持つul要素 の li要素
+		// listBdクラスを持つul要素 の li要素
 		$('ul.listBd > li').each(function(){
 
 			var _$p_ttl = $(this).find('p.ttl');
@@ -55,15 +60,12 @@ dialog.matches('ニュース',
 			var _$span_cp = _$p_source.find('span.cp');
 
 		 	// ニュースタイトル
-			//var ttlText = $(this).find('span.ttl').text();
-			// var ttlText = $(this).text();
 			var ttlText = _$a.text();
 
 			// ニュースソース
 			var sourceText = _$span_cp.text();
 
 			// a要素に設定されたリンクURL取得
-			// var linkUrl = $(this).attr('href');
 			var linkUrl = _$a.attr('href');
 
 			// ニュースタイトル ソース リンクURLの順で結果文字列に追加
@@ -118,19 +120,6 @@ dialog.matches('4gamer',
 	}
 );
 
-var fncTellMe = function(session){
-		if(!session.userData.first){
-			session.beginDialog('/talk/first');
-
-		} else if(! session.userData.seccond){
-			session.beginDialog('/talk/seccond');
-
-		} else {
-			session.send('I catch ' + session.userData.first + " and " + session.userData.seccond + "!");
-		
-		}
-	};
-
 dialog.matches('会話しよう',[
 	function(session){
 		fncTellMe(session);		
@@ -166,8 +155,20 @@ bot.add('/talk/seccond',[
 	}
 ]);
 
+var fncTellMe = function(session){
+		if(!session.userData.first){
+			session.beginDialog('/talk/first');
 
-dialog.matches((['本']), function(session){
+		} else if(! session.userData.seccond){
+			session.beginDialog('/talk/seccond');
+
+		} else {
+			session.send('I catch ' + session.userData.first + " and " + session.userData.seccond + "!");
+		
+		}
+	};
+
+dialog.matches('コミック', function(session){
 
 	var dtBase = new Date();
 
@@ -194,7 +195,6 @@ dialog.matches((['本']), function(session){
 	rst_book_text += "---\n\r";
 
 	// ブックサーチから今月分のページへアクセス
-	// client.fetch('http://www.bookservice.jp/layout/bs/common/html/schedule/1606c.html',  function (err, $, res) {
 	client.fetch('http://www.bookservice.jp/layout/bs/common/html/schedule/' + url_now + 'c.html',  function (err, $, res) {
 
 		_$tBody = $('tBody');
@@ -232,71 +232,6 @@ dialog.matches((['本']), function(session){
 
 	});
 });
-
-
-	  // _$tBody = $('tBody');
-
-	//   // 冊数を数える
-	//   len = _$tBody.children().length;
-
-	//   session.send(formatted_full + "には以下の本が発売されます。");
-
-	//   // tBodyの子要素であるtrを指定してeachで回している
-	//   $(_$tBody.children()).each(function(){
-
-	//   	// thisはこの位置ではtrのこと。その中からtd要素を検索している
-	// 	_$td = $(this).find('td');
-
-	// 	// 検索したtd要素をテキストにパース
-	// 	td = _$td.text();
-
-	// 	// 出版社
-	// 	td_publisher = _$td.eq(0).text();
-
-	// 	// 日付
-	// 	td_date = _$td.eq(1).text();
-
-	// 	// レーベル
-	// 	td_rabel = _$td.eq(2).text();
-
-	// 	// タイトル
-	// 	td_title = _$td.eq(3).text();
-
-	// 	// 作者
-	// 	td_writeby = _$td.eq(4).text();
-
-	// 	// 値段
-	// 	td_value = _$td.eq(5).text();
-
-	// 	// ISBNコード
-	// 	td_isbn = _$td.eq(6).text();
-
-	// 	// 出力文字列へ整形
-	// 	text_result = "出版社:" + td_publisher
-	// 					+ " レーベル:" + td_rabel
-	// 					+ " タイトル:" + td_title
-	// 					+ " 作者:" + td_writeby;
-
-	// 	// 取得した日付と実行日を比較し、当日発売であればコンソールへ出力
-	//   	if(td_date==formatted){
-	//   		// console.log(text_result);
-	// 	  	// console.log(td);
- 
-	// 	  	session.send(text_result);
-
-	//   	}
-
-	//   });
-	// });
-
-// });
-
-// Talk Method
-
-// bot.add('/', function (session) {
-//    session.send('Hello World'); 
-// });
-
 
 bot.add('/', dialog);
 
